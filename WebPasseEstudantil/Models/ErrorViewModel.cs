a4d5311631dc3b38f62cfabd1208c28b0105ce92
Gabriel Fachola <gfachola1@gmail.com>
2024-04-18 22:11:23 -0300

    Add project files.

---
+++ WebPasseEstudantil/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebPasseEstudantil.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
